0b22660a821794c332448d3fad8182edaf287534
jesusrj <reginaldo.jesus@gmail.com>
2018-02-20 07:18:53 -0300

    packages

--- curso_node/noticias.js
+++ curso_node/noticias.js
@@ -1,7 +1,17 @@
 var http = require('http');
 
 var server = http.createServer(function(req, res){
-  res.end("<html><body>Portal de notícias</body></html>");
+  var categoria = req.url;
+
+  if (categoria == '/tecnologia') {
+    res.end("<html><body>Portal de notícias de Tecnologia</body></html>");
+  } else if (categoria == '/moda') {
+    res.end("<html><body>Portal de notícias de Moda</body></html>");
+  } else if (categoria == '/beleza') {
+    res.end("<html><body>Portal de notícias de Beleza</body></html>");
+  } else {
+    res.end("<html><body>Portal de notícias</body></html>");
+  }
 });
 
 server.listen(3000);
