c1b9c2571ec6477bbb0f41ee3faa76e6557d9257
jesusrj <reginaldo.jesus@gmail.com>
2018-02-21 07:30:35 -0300

    Modulos

---
+++ curso_node/app.js
@@ -0,0 +1,21 @@
+var app = require('express')();
+var msg = require('./mod_teste')();
+
+app.set('view engine', 'ejs')
+
+app.get('/', function(req, res){
+  res.render("home/index");
+});
+
+app.get('/formulario_inclusao_noticia', function(req, res){
+  res.render("admin/form_add_noticia");
+});
+
+app.get('/noticias', function(req, res){
+  res.render("noticias/noticias");
+});
+
+app.listen(3000, function(){
+  console.log('Server is running on port 3000.');
+  console.log(msg);
+});
---
+++ curso_node/mod_teste.js
@@ -0,0 +1,4 @@
+module.exports = function(){
+  var msg = "Este módulo contêm apenas uma string!";
+  return msg;
+}
