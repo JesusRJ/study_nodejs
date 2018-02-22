4d016321709c1a24e5968aa77d38209a0d01f9ee
jesusrj <reginaldo.jesus@br.unisys.com>
2018-02-22 18:05:10 +0000

    Inclusao no banco de dados

--- curso_node/config/dbConnection.js
+++ curso_node/config/dbConnection.js
@@ -1,8 +1,9 @@
+var os = require('os');
 var mysql = require('mysql');
 
 var connMysql = function(){
   return mysql.createConnection({
-    host: "localhost",
+    host: (os.hostname() == "br-jesusrj" ? "192.168.99.100" : "localhost"),
     user: "portal",
     password: "portal",
     database: "portal_noticias"
