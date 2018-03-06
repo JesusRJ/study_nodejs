3e206e0a4dddaa31a75f98bc0702c82a2127cbc5
jesusrj <reginaldo.jesus@gmail.com>
2018-03-06 07:00:22 -0300

    validação de usuário e senha

--- mmorpg_got/app/controllers/index.js
+++ mmorpg_got/app/controllers/index.js
@@ -1,3 +1,20 @@
 module.exports.home = function(application, req, res) {
-  res.render('index');
+  res.render('index', {validacao: {}});
+}
+
+module.exports.autenticar = function(application, req, res) {
+  
+  var dadosForm = req.body;
+
+  req.assert('usuario', 'Usuário obrigatório').notEmpty();
+  req.assert('senha', 'Senha obrigatório').notEmpty();
+
+  var erros = req.validationErrors();
+
+  if(erros) {
+    res.render('index', {validacao: erros});
+    return;
+  }
+
+  res.render('index', {validacao: erros});
 }
